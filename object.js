0a8c5ecb92f417012553320b6aba8af2abb9655c
Chiji-ndu <iwuchukwuprecious2003@gmail.com>
2025-02-03 01:40:54 -0800

    Add files via upload
    this is my assignment

---
+++ object.js
@@ -0,0 +1,48 @@
+// Object Asssignment
+
+// Task 1
+
+const movies = {
+    movie1: {
+        "title": "Blacklist",
+        "author": "Raymond Reddington",
+        "description": "Crime and love",
+        "yearOfRelease": "2004"
+    },
+    movie2: {
+        "title": "Prison Break",
+        "author": "Micheal Scoffield",
+        "description": "Crime and love",
+        "yearOfRelease": "2003"
+    },
+    movie3: {
+        "title": "Game of Thrones",
+        "author": "Ned Stark",
+        "description": "Crime and War",
+        "yearOfRelease": "2010"
+    },
+    movie4: {
+        "title": "Peaky Blinders",
+        "author": "Thomas Shelby",
+        "description": "Crime and love",
+        "yearOfRelease": "2012"
+    },
+    movie5: {
+        "title": "Alchemy of Souls ",
+        "author": "Jang Uk",
+        "description": "Crime and love",
+        "yearOfRelease": "2022"
+    },
+    movie6: {
+        "title": "The Day of the Jackal",
+        "author": "the Jackal",
+        "description": "Crime and action",
+        "yearOfRelease": "2024"
+    }
+            
+}
+console.log(movies);
+
+delete(movies.movie3);
+
+console.log(movies);
